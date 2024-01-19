79fc72dcfc66f4f252c1b3f22fb0d0580214df9d
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-01-19 12:27:54 +0200

    Complete task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+    event.preventDefault();
+    const formEvent = event.target;
+    const email = event.target.elements.email.value;
+    const password = event.target.elements.password.value;
+
+    if(email === '' || password === '') {
+        return alert('Please try again')
+    }
+    
+    const objectValue = {
+        email,
+        password
+    }
+
+    console.log(objectValue)
+    formEvent.reset();
+}
 
