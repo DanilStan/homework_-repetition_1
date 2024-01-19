cd01d3a57e7762e0c0866f20027a305649f00718
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-01-19 12:46:04 +0200

    Complete task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,19 @@
+const bodyRef = document.querySelector('body');
+const buttonChangeColor = document.querySelector('.change-color');
+const colorText = document.querySelector('.color')
+// bodyRef.style.backgroundColor = "blue";
+
+buttonChangeColor.addEventListener('click', onClickButton)
+
+function onClickButton() {
+  const colorValue = getRandomHexColor()
+  bodyRef.style.backgroundColor = colorValue;
+  colorText.textContent = colorValue;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
